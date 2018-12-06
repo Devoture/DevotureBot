95d3d332f5cfec89336b4e68b18b244cfb8546dc
Tom Sauer <tom@exluto.com>
2018-12-06 17:38:51 -0500

    adding dong

--- bot.js
+++ bot.js
@@ -1,24 +1,29 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
+
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+    token: auth.token,
+    autorun: true
 });
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
+    // tell bot to execute commands
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
@@ -31,9 +36,14 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
-                });
+            });
             break;
-            // Just add any case commands if you want to..
+
+            case 'ding':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Dong!'
+                });
          }
      }
 });
